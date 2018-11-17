b4967d82af6507c3f41c044d28dcef63df7f83dd
saigontech <nguyenquan263@gmail.com>
2018-11-17 08:34:55 +0700

    morning 17/11/2018

--- captive_portal_backend/app.py
+++
@@ -1,4 +0,0 @@
-from controllers.info_controller import app
-
-if __name__ == '__main__':
-    app.run(debug=True,port=8080 ,threaded=True, host='0.0.0.0')
--- captive_portal_backend/test.py
+++
@@ -1,2 +0,0 @@
-phone = '0966362138'
-print('84'+phone[1:len(phone) - 1])
